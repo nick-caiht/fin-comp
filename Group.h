649c649e6cfed567c2731de155e4a2bbeb00cb26
ulc5410 <100205060@nccu.edu.tw>
2017-04-28 22:59:43 -0400

    Update Group.h

--- Group.h
+++ Group.h
@@ -7,13 +7,13 @@
 
 class Group {
 private:
-	std::map<std::string, Stock> stocks;
+	std::multimap<std::string, Stock> stocks;
 	std::vector<std::string> map_keys;
 	std::vector<double> AAR = std::vector<double>(91);
 	std::vector<double> CAAR = std::vector<double>(91);
 public:
 	Group();
-	Group(const Group& g_p); //Realize the copy constructor
+	Group(const Group& g_p); 
 	~Group();
 	const std::vector<double>& GetAAR();
 	const std::vector<double>& GetCAAR();
@@ -21,6 +21,6 @@ public:
 	const std::vector<std::string>& GetMapKeys() const;
 	const int GetSize() const;
 	bool PushStock(const Stock& s);
-	bool Compute(); // wrong calculation
+	bool Compute();
 };
 
